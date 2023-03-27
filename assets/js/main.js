091087b734177ab8aaca200968e3cb1ffd381a8e
Kethlyn <kethdiniz06@gmail.com>
2023-03-27 11:15:32 -0300

    adicionando infinite scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,12 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 250
-const limit = 10
+const limit = 25
 let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}" >${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
 
+
+//Requisita mais pokemons a API
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
@@ -30,6 +17,7 @@ function loadPokemonItens(offset, limit) {
 
 loadPokemonItens(offset, limit)
 
+//Carrega mais pokemons ao clicar
 loadMoreButton.addEventListener('click', () => {
     offset += limit
     const qtdRecordsWithNexPage = offset + limit
@@ -43,3 +31,35 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+//Infinite Scroll - Carrega mais pokemons ao final da página
+window.addEventListener('scroll', () => {
+    if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+        offset += limit
+        const qtdRecordsWithNexPage = offset + limit
+
+        if (qtdRecordsWithNexPage >= maxRecords) {
+            const newLimit = maxRecords - offset
+            loadPokemonItens(offset, newLimit)
+        } else {
+            loadPokemonItens(offset, limit)
+        }
+    }
+})
+
+//Converte os pokemons em uma lista
+function convertPokemonToLi(pokemon) {
+    return `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}" >${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}"
+                         alt="${pokemon.name}">
+                </div>
+            </li>
+        `
+}
